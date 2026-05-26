84b4720a0f0135350f2ed946b7d1f106f328e95e
spaced <73479844+spacedouut@users.noreply.github.com>
2026-05-25 20:00:14 -0400

    please just work

--- .github/scripts/build-hyperion.sh
+++ .github/scripts/build-hyperion.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 ARCH=$1
-DOCKER_ARGS=""
 
+# Log into GHCR so the builder can push via the host Docker socket.
+# We don't pass --docker-user/--docker-password to the builder because
+# its internal docker login defaults to Docker Hub and will fail with
+# GitHub tokens.
 if [ -n "${DOCKER_USER:-}" ] && [ -n "${DOCKER_PASSWORD:-}" ]; then
-    DOCKER_ARGS="--docker-user ${DOCKER_USER} --docker-password ${DOCKER_PASSWORD}"
+    echo "${DOCKER_PASSWORD}" | docker login ghcr.io -u "${DOCKER_USER}" --password-stdin
 fi
 
 docker run --rm --privileged \
@@ -12,6 +15,5 @@ docker run --rm --privileged \
     -v ${GITHUB_WORKSPACE:-$(PWD)}/addon-hyperion-ng:/data \
     homeassistant/amd64-builder \
     --target /data \
-    ${DOCKER_ARGS} \
     --no-latest \
     --${ARCH:-all}
